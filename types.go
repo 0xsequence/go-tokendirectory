00397a42a88f9f201e750bf4880ca5ceac1e88fb
Alex Guerrieri <ag@horizon.io>
2023-05-04 17:14:03 +0200

    add standard field

--- types.go
+++ types.go
@@ -15,10 +15,10 @@ type TokenList struct {
 }
 
 type ContractInfo struct {
-	ChainID uint64 `json:"chainId"`
-	Address string `json:"address"`
-	Name    string `json:"name"`
-	// Standard   string `json:"standard"`
+	ChainID    uint64 `json:"chainId"`
+	Address    string `json:"address"`
+	Name       string `json:"name"`
+	Standard   string `json:"standard"`
 	Type       string `json:"type"` // NOTE: a duplicate of standard to normalize with other Sequence payloads
 	Symbol     string `json:"symbol,omitempty"`
 	Decimals   uint64 `json:"decimals"`
