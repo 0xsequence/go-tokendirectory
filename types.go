5fb80f12a70051cae4fc00e8e6aede8577349b61
Alex Guerrieri <ag@horizon.io>
2024-03-20 18:17:34 +0100

    Featured as int

--- types.go
+++ types.go
@@ -1,6 +1,8 @@
 package tokendirectory
 
-import "time"
+import (
+	"time"
+)
 
 // TokenList structure based on https://raw.githubusercontent.com/Uniswap/token-lists/main/test/schema/example.tokenlist.json
 type TokenList struct {
@@ -15,26 +17,27 @@ type TokenList struct {
 }
 
 type ContractInfo struct {
-	ChainID uint64 `json:"chainId"`
-	Address string `json:"address"`
-	Name    string `json:"name"`
-	// Standard   string `json:"standard"`
-	Type       string `json:"type"` // NOTE: a duplicate of standard to normalize with other Sequence payloads
-	Symbol     string `json:"symbol,omitempty"`
-	Decimals   uint64 `json:"decimals"`
-	LogoURI    string `json:"logoURI,omitempty"`
-	Extensions struct {
-		Link                  string   `json:"link,omitempty"`
-		Description           string   `json:"description,omitempty"`
-		OgImage               string   `json:"ogImage,omitempty"`
-		OriginChainID         uint64   `json:"originChainId,omitempty"`
-		OriginAddress         string   `json:"originAddress,omitempty"`
-		Blacklist             bool     `json:"blacklist,omitempty"`
-		ContractABIExtensions []string `json:"contractABIExtensions,omitempty"`
-		Featured              bool     `json:"featured,omitempty"`
-		Mute                  bool     `json:"mute,omitempty"`
-		Verified              bool     `json:"verified"`
-		VerifiedBy            string   `json:"verifiedBy,omitempty"`
-	} `json:"extensions"`
-	ContentHash uint64 `json:"-"`
+	ChainID     uint64                `json:"chainId"`
+	Address     string                `json:"address"`
+	Name        string                `json:"name"`
+	Type        string                `json:"type"`
+	Symbol      string                `json:"symbol,omitempty"`
+	Decimals    uint64                `json:"decimals"`
+	LogoURI     string                `json:"logoURI,omitempty"`
+	Extensions  ContractInfoExtension `json:"extensions"`
+	ContentHash uint64                `json:"-"`
+}
+
+type ContractInfoExtension struct {
+	Link                  string   `json:"link,omitempty"`
+	Description           string   `json:"description,omitempty"`
+	OgImage               string   `json:"ogImage,omitempty"`
+	OriginChainID         uint64   `json:"originChainId,omitempty"`
+	OriginAddress         string   `json:"originAddress,omitempty"`
+	Blacklist             bool     `json:"blacklist,omitempty"`
+	ContractABIExtensions []string `json:"contractABIExtensions,omitempty"`
+	Featured              int32    `json:"featured,omitempty"`
+	Mute                  bool     `json:"mute,omitempty"`
+	Verified              bool     `json:"verified"`
+	VerifiedBy            string   `json:"verifiedBy,omitempty"`
 }
